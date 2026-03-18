23a0736cc16636dcaed5c8c73e906360a7aa9397
soumit02 <soumitdey2002@gmail.com>
2026-03-18 22:56:06 +0600

    Initial commit: Added Python code, gitignore and README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# smart-profile-manager
+# Smart Profile & Skill Manager 
+
+This is a simple interactive Command-Line Interface (CLI) project built with Python. It demonstrates the practical use of core data structures like Dictionaries, Tuples, Lists, and Sets.
+
+## Features:
+* Add user profiles with fixed data (Birth Year, Blood Group) using **Tuples**.
+* Manage multiple projects using **Lists**.
+* Store unique skills and prevent duplicates automatically using **Sets**.
+* Store everything in an organized way using a **Dictionary**.
+
+## How to Run:
+1. Clone this repository.
+2. Run `python main.py` in your terminal.
---
+++ main.py
@@ -0,0 +1,71 @@
+# created a dictionary where everyone's profile will be saved
+profiles = {}
+
+while True:
+    # Interface design
+    print("\n" + "="*40)
+    print("   SMART PROFILE & SKILL MANAGER")
+    print("="*40)
+    print("1. Add New Profile")
+    print("2. View a Profile")
+    print("3. Exit")
+    print("="*40)
+    
+    choice = input("Select your choice (1/2/3): ")
+    
+    if choice == '1':
+        name = input("\nEnter the name: ").title()
+        
+        # Birth year and blood group
+        dob = input("Enter the Birth Year: ")
+        blood_group = input("Enter the Blood Group: ")
+        basic_info = (dob, blood_group) 
+        
+        # Project name
+        projects_input = input("Write your projects name: ")
+        projects_list = projects_input.split(",") 
+        
+        # skills
+        skills_input = input("Write down your skills.: ").lower()
+        skills_set = set(skills_input.split(",")) 
+        
+        # Saving all data together in one profile
+        profiles[name] = {
+            "Basic Info": basic_info,
+            "Projects": [proj.strip() for proj in projects_list], # List comprehension for clean spacing
+            "Skills": {skill.strip() for skill in skills_set} # Set comprehension
+        }
+        print(f"\n{name}'s profile has been successfully created!")
+
+    elif choice == '2':
+        search_name = input("\nEnter the name of the person you want to view the profile of: ").title()
+        
+        # Checking whether a profile exists
+        if search_name in profiles:
+            user_data = profiles[search_name]
+            print("\n" + "-"*30)
+            print(f"Profile of {search_name}")
+            print("-"*30)
+            # Showing data from a tuple
+            print(f"Birth Year: {user_data['Basic Info'][0]}")
+            print(f"Blood Group: {user_data['Basic Info'][1]}")
+            
+            # Displaying data from List
+            print("\nProjects:")
+            for project in user_data['Projects']:
+                print(f"  - {project}")
+                
+            # Displaying data from Set
+            print("\nSkills:")
+            for skill in user_data['Skills']:
+                print(f"   {skill}")
+            print("-"*30)
+        else:
+            print(f"Sorry, no profile was found with the name '{search_name}'.")
+
+    elif choice == '3':
+        print("\nThe program is closing. Thank you!")
+        break
+        
+    else:
+        print("\nIncorrect input! Please press 1, 2 or 3.")
